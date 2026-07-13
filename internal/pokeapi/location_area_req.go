ceca25d786ef7ccb072d64da48c96bb4b890d5ce
Santhosh VS <santhosh02vs@gmail.com>
2026-07-13 23:53:14 +0530

    implement ListLocationAreas function to fetch location areas from PokeAPI

---
+++ internal/pokeapi/location_area_req.go
@@ -0,0 +1,42 @@
+package pokeapi
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+)
+
+func (c *Client) ListLocationAreas() (LocationAreasResp, error) {
+	endpoint := "/location-area-test"
+	fullURL := baseURL + endpoint
+
+	req, err := http.NewRequest("GET", fullURL, nil)
+	if err != nil {
+		return LocationAreasResp{}, err
+	}
+
+	resp, err := c.httpClient.Do(req)
+	if err != nil {
+		return LocationAreasResp{}, err
+	}
+
+	if resp.StatusCode > 399 {
+		return LocationAreasResp{}, fmt.Errorf("Bad status code: %v", resp.StatusCode)
+	}
+
+	defer resp.Body.Close()
+
+	dat, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return LocationAreasResp{}, err
+	}
+
+	locationAreasResp := LocationAreasResp{}
+	err = json.Unmarshal(dat, &locationAreasResp)
+	if err != nil {
+		return LocationAreasResp{}, err
+	}
+
+	return locationAreasResp, nil
+}
